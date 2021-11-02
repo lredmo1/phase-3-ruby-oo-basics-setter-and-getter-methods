cacafe19cc06330c88252be8adf1a5cf154fd43e
Lisa <redlisa125@gmail.com>
2021-11-01 17:02:07 -1000

    commit tree 2bb6c49cb39ad767cd5d134d6120bd7951b14166

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    def name=(value)
+        @name = value
+    end
+
+    def name
+        @name
+    end
+    
+    def breed=(value)
+        @breed = value
+    end
+
+    def breed
+        @breed
+    end
+    
 
 end
